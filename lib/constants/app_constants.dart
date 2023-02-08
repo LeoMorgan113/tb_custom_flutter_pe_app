f98cb0e505a251697b88034b94f9e06019571765
LeoMorgan113 <mandragora1783@gmail.com>
2023-02-08 15:37:10 +0200

    Removing unused IPs

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,8 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://10.7.3.172:8080/'; //mine
-  // static final thingsBoardApiEndpoint = 'http://172.18.77.32:8080/'; // choco
-  // static final thingsBoardApiEndpoint = 'http://login.darkblueiot.com/';
-  // static final thingsBoardApiEndpoint = 'https://thingsboard.cloud/';
+  static final thingsBoardApiEndpoint = 'http://172.18.77.32:8080/';
   static final thingsboardOAuth2CallbackUrlScheme =
       'org.thingsboard.pe.app.auth';
 
