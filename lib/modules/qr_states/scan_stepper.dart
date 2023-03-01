735a304e3f18fab30265ceb6caaf81a530eb7c50
LeoMorgan113 <mandragora1783@gmail.com>
2023-03-01 17:44:34 +0200

    Wedel: removed zebra

--- lib/modules/qr_states/scan_stepper.dart
+++ lib/modules/qr_states/scan_stepper.dart
@@ -1,18 +1,20 @@
-import 'dart:async';
 import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:thingsboard_app/core/entity/entities_base.dart';
 import 'package:thingsboard_app/modules/qr_states/states/scan_finised.dart';
 import 'package:thingsboard_app/modules/qr_states/states/scan_item.dart';
 import 'package:thingsboard_pe_client/thingsboard_client.dart';
 
+// import '../../widgets/tb_progress_indicator.dart';
 import '../../core/context/tb_context.dart';
 import '../../widgets/tb_progress_indicator.dart';
 import 'states/scan_order.dart';
 import 'states/scan_user.dart';
 import 'package:http/http.dart' as http;
-import 'package:zebrascanner/zebrascanner.dart';
 
 enum Types { USER, ORDER, ITEM }
 
@@ -43,67 +45,10 @@ class _ScanStepperState extends State<ScanStepper> {
   late bool itemIdSet = false;
   bool _loading = true;
 
-  // Zebra app
-  String _platformVersion = 'Unknown';
-  dynamic map;
-  StreamSubscription? subscription;
-  String? eventData = "";
-
-
   @override
   void initState(){
     super.initState();
-    // to get device OS Version
-    initPlatformState();
-    // to get Barcode Events from Zebra Scanner
-    initBarcodeReceiver();
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (subscription != null) {
-      subscription!.cancel();
-    }
-  }
-
-
-  initBarcodeReceiver() {
-    subscription = Zebrascanner.getBarCodeEventStream.listen((barcodeData) {
-      setState(() {
-        map = barcodeData;
-        var _list = map.values.toList();
-
-        // print(map);
-        // // Barcode
-        // print(_list[0]);
-        // // BarcodeType
-        // print(_list[1]);
-        // // ScannerId
-        // print(_list[2]);
-
-        eventData = _list[0] + "-" + _list[1] + "-" + _list[2];
-      });
-    });
-  }
-
-  Future<void> initPlatformState() async {
-    String platformVersion;
-
-    try {
-      platformVersion = await Zebrascanner.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -113,138 +58,131 @@ class _ScanStepperState extends State<ScanStepper> {
         children: [
           Expanded(
               child: Stepper(
-            type: StepperType.horizontal,
-            physics: ScrollPhysics(),
-            currentStep: _currentStep,
-            onStepContinue: lastField ? null : continued,
-            onStepCancel: _currentStep == 0 ? null : cancel,
-            controlsBuilder: (context, details) {
-              return Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: lastField
-                      ? Row(
-                          children: [
-                            if(!_loading)
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text("Close"),
-                                ),
+                type: StepperType.horizontal,
+                physics: ScrollPhysics(),
+                currentStep: _currentStep,
+                onStepContinue: lastField ? null : continued,
+                onStepCancel: _currentStep == 0 ? null : cancel,
+                controlsBuilder: (context, details) {
+                  return Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: lastField
+                          ? Row(
+                        children: [
+                          if(!_loading)
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Close"),
                               ),
-                          ],
-                        )
-                      : Row(
-                          children: [
-                            if (_currentStep != 0)
-                              Expanded(
-                                child: TextButton(
-                                    onPressed: details.onStepCancel,
-                                    child: const Text(
-                                      "Back",
-                                    )),
-                              ),
-                            const SizedBox(width: 12),
-                            if (_currentStep == 0)
-                              // User scan
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: isUserValid ?
-                                      details.onStepContinue
-                                      : null ,
-                                  child: Text("Next"),
-                                ),
-                              )
-                            else if (_currentStep == 1)
-                              // order scan
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: orderIdSet
-                                      ? details.onStepContinue
-                                      : null,
-                                  child: Text("Next"),
-                                ),
-                              )
-                            else
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: itemIdSet ?
-                                      details.onStepContinue
-                                      : null,
-                                  child: const Text("Send request"),
-                                ),
-                              ),
-
-                          ],
-                        ),);
-            },
-            steps: <Step>[
-              Step(
-                title: Text('User'),
-                // content: _buildScanUser(),
-                content: Column(
-                  children: [
-                    ScanUser(
-                        userQrCode: getUserQrCode,
-                        scanQrCodeCallback: openBarcodeScreen,
-                        userValid: isUserValid,
-                    ),
-                    Text('Scanned Barcode/Qrcode ' + eventData!,
-                      style: TextStyle(color: Colors.green),)
-                  ],
-                ),
-                isActive: _currentStep == 0,
-                state:
-                    _currentStep > 0 ? StepState.complete : StepState.disabled,
-              ),
-              Step(
-                title: Text('Order'),
-                content: ScanOrder(
-                    orderQrCode: getOrderQrCode,
-                    scanQrCodeCallback: openBarcodeScreen,
-                    continueStep: continued,
-                    commentCallback: setComment,),
-                isActive: _currentStep == 1,
-                state:
-                    _currentStep > 1 ? StepState.complete : StepState.disabled,
-              ),
-              Step(
-                title: Text('Item'),
-                content: ScanItem(
-                    itemQrCode: getItemQrCode,
-                    scanQrCodeCallback: openBarcodeScreen,
-                    itemCountCallback: setItemQuantity),
-                isActive: _currentStep == 2,
-                state:
-                    _currentStep >= 2 ? StepState.complete : StepState.disabled,
-              ),
-              Step(
-                title: Text('Finish'),
-                content:
-                _loading ?
-                      SizedBox(
-                        height: 500,
-                        child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                                TbProgressIndicator(widget.tbContext),
-                                Padding(padding: EdgeInsets.only(top: 15)),
-                                Text('Processing...',
-                                style: TextStyle(
-                                  fontSize: 18
-                                ),)
-                            ]),
+                            ),
+                        ],
                       )
-                    : ScanFinished(status: requestSuccess),
-                isActive: _currentStep == 3,
-                state:
+                          : Row(
+                        children: [
+                          if (_currentStep != 0)
+                            Expanded(
+                              child: TextButton(
+                                  onPressed: details.onStepCancel,
+                                  child: const Text(
+                                    "Back",
+                                  )),
+                            ),
+                          const SizedBox(width: 12),
+                          if (_currentStep == 0)
+                          // User scan
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: isUserValid ?
+                                details.onStepContinue
+                                    : null ,
+                                child: Text("Next"),
+                              ),
+                            )
+                          else if (_currentStep == 1)
+                          // order scan
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: orderIdSet
+                                    ? details.onStepContinue
+                                    : null,
+                                child: Text("Next"),
+                              ),
+                            )
+                          else
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: itemIdSet ?
+                                details.onStepContinue
+                                    : null,
+                                child: const Text("Send request"),
+                              ),
+                            ),
+                        ],
+                      ));
+                },
+                steps: <Step>[
+                  Step(
+                    title: Text('User'),
+                    // content: _buildScanUser(),
+                    content: ScanUser(
+                      userQrCode: getUserQrCode,
+                      scanQrCodeCallback: scanQRCode,
+                      userValid: isUserValid,
+                    ),
+                    isActive: _currentStep == 0,
+                    state:
+                    _currentStep > 0 ? StepState.complete : StepState.disabled,
+                  ),
+                  Step(
+                    title: Text('Order'),
+                    content: ScanOrder(
+                      orderQrCode: getOrderQrCode,
+                      scanQrCodeCallback: scanQRCode,
+                      continueStep: continued,
+                      commentCallback: setComment,),
+                    isActive: _currentStep == 1,
+                    state:
+                    _currentStep > 1 ? StepState.complete : StepState.disabled,
+                  ),
+                  Step(
+                    title: Text('Item'),
+                    content: ScanItem(
+                        itemQrCode: getItemQrCode,
+                        scanQrCodeCallback: scanQRCode,
+                        itemCountCallback: setItemQuantity),
+                    isActive: _currentStep == 2,
+                    state:
+                    _currentStep >= 2 ? StepState.complete : StepState.disabled,
+                  ),
+                  Step(
+                    title: Text('Finish'),
+                    content:
+                    _loading ?
+                    SizedBox(
+                      height: 500,
+                      child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TbProgressIndicator(widget.tbContext),
+                            Padding(padding: EdgeInsets.only(top: 15)),
+                            Text('Processing...',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),)
+                          ]),
+                    )
+                        : ScanFinished(status: requestSuccess),
+                    isActive: _currentStep == 3,
+                    state:
                     _currentStep >= 3 ? StepState.complete : StepState.disabled,
-              ),
-            ],
-          )),
+                  ),
+                ],
+              )),
         ],
       ),
     );
@@ -274,12 +212,12 @@ class _ScanStepperState extends State<ScanStepper> {
 
 
   Future<dynamic> checkUser(String userId,{RequestConfig? requestConfig}) async{
-    // var id = '296adab0-852b-11ed-8927-6933c7590d09';
+    // var id = 'bfc2fbf0-86b6-11ed-a5ef-ff73adaaed5c';
     var id = userId;
     try{
       var response = await widget.tbClient.
       get("/api/wedel/user?azureId=${id.toString()}");
-      print(response);
+
       if(response.statusCode == 200){
         Map<String, dynamic> jsonMap  = jsonDecode(response.toString());
         var userId = jsonMap['userId']['id'];
@@ -291,21 +229,6 @@ class _ScanStepperState extends State<ScanStepper> {
     }
   }
 
-  //SCANNER FOR ZEBRA DEVICE
-  Future<void> openBarcodeScreen(Types type) async {
-    String? result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      result = await Zebrascanner.barcodeScreen;
-
-      setQrCode(result, type);
-    } on PlatformException {
-      result = 'Failed to open the screen';
-    }
-  }
-
-
-  // SCANNER FOR SMARTPHONE
   void scanQRCode(Types type) async {
     try {
       final qrCode = await FlutterBarcodeScanner.scanBarcode(
@@ -313,7 +236,7 @@ class _ScanStepperState extends State<ScanStepper> {
       if (!mounted) return;
 
       setQrCode(qrCode, type);
-    //
+
     } on PlatformException {
       getResult = 'Failed to scan QR Code.';
     }
@@ -356,24 +279,26 @@ class _ScanStepperState extends State<ScanStepper> {
   }
 
   continued() {
+    // print('getUserQrCode:  $getUserQrCode, getItemQrCode $getItemQrCode,'
+    //     '\ngetOrderQrCode $getOrderQrCode');
 
     _currentStep < 4
         ? setState(() {
-            _currentStep += 1;
-            if (_currentStep == 3) {
-              body = {
-                "itemType": getItemQrCode,
-                // "itemType": "6cd0d1a0-8c20-11ed-a932-f5796fe3fac8",
-                "orderId": getOrderQrCode.isNotEmpty ? getOrderQrCode : comment,
-                "userId": userId,
-                "itemCount": itemCount
-              };
-              takeItemRequest(body);
-              lastField = true;
-            } else {
-              lastField = false;
-            }
-          })
+      _currentStep += 1;
+      if (_currentStep == 3) {
+        body = {
+          "itemType": getItemQrCode,
+          // "itemType": "6cd0d1a0-8c20-11ed-a932-f5796fe3fac8",
+          "orderId": getOrderQrCode.isNotEmpty ? getOrderQrCode : comment,
+          "userId": userId,
+          "itemCount": itemCount
+        };
+        takeItemRequest(body);
+        lastField = true;
+      } else {
+        lastField = false;
+      }
+    })
         : null;
   }
 
@@ -393,7 +318,7 @@ class _ScanStepperState extends State<ScanStepper> {
     });
 
     var response = await widget.tbClient.post('/api/wedel/take',
-        data: body,
+      data: body,
     );
 
     if(response.statusCode == 200){
